3d7aa402a5cbe1bbe0c5cb26621603f4d76860fe
Matt Eland <matt.eland@gmail.com>
2021-09-17 11:36:47 -0400

    M1D5 Causing pain

--- module-1/05_Command_Line_Programs/lecture-student/dotnet/DiscountCalculator/MergeTest.cs
+++ module-1/05_Command_Line_Programs/lecture-student/dotnet/DiscountCalculator/MergeTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +8,7 @@ namespace DiscountCalculator
     {
         public string GenerateGreetings()
         {
-            string name = "YOURNAME HERE";
+            string name = "Matt Eland";
 
             return "Hello " + name;
         }
