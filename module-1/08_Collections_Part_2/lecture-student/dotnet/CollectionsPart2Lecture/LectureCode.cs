02a31cd83308e28b2da100fecc48419b1a5003ca
Matt Eland <matt.eland@gmail.com>
2021-09-21 16:05:59 -0400

    M1D7 - Lecture Setup, Exercises

---
+++ module-1/08_Collections_Part_2/lecture-student/dotnet/CollectionsPart2Lecture/LectureCode.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionsPart2Lecture
+{
+    public class LectureCode
+    {
+		public void Start()
+		{
+			// ----------------------------------------
+			// HashSets
+			// ----------------------------------------
+			HashSetExamples();
+
+			// ----------------------------------------
+			// Dictionaries
+			// ----------------------------------------
+			DictionarySportsJerseyExample();
+
+			// ----------------------------------------
+			// Example Program: Count the number of times a coin flips heads or tails
+			// ----------------------------------------
+			DictionaryCoinFlipExample();
+
+			// ----------------------------------------
+			// Less Common Cases with Dictionaries
+			// ----------------------------------------
+			DictionaryItemPricesExample();
+
+			// ----------------------------------------
+			// Example Program: Check to see if a username and password match
+			// ----------------------------------------
+			UsernamePasswordExample();
+		}
+
+		private void DictionaryItemPricesExample()
+		{
+			// Declaring and initializing a Dictionary (one-line version)
+
+			// Iterate through the keys and key-value pairs in the Dictionary (You will RARELY do this)
+		}
+
+		private void DictionaryCoinFlipExample()
+		{
+
+		}
+
+		private void DictionarySportsJerseyExample()
+		{
+			// Declaring and initializing a Dictionary of Jersey numbers and sports players
+
+			// Adding an item to a Dictionary
+
+			// Retrieving an item from a Dictionary by its key
+
+			// Updating an item in a Dictionary
+
+			// Check to see if a key already exists
+
+			// Remove an element from the Dictionary
+		}
+
+		private void HashSetExamples()
+		{
+			// Declaring a hash set for tracking E-Mail addresses
+
+			// Adding to hash sets
+
+			// Removing from hash sets
+
+			// Checking if something is in a hash set
+
+			// Looping over a hash set (you won't do this too much)
+		}
+
+		private void UsernamePasswordExample()
+		{
+
+		}
+
+		private bool IsPasswordValid(string username, string password)
+		{
+			// NOTE: This is a horrible example of password security, but is a good example for teaching dictionaries
+			// We will revisit password security in module 2.
+
+			return false;
+		}
+	}
+}
