388a231bd0acb36ce763e221075d653423a3a7a4
Matt Eland <matt.eland@gmail.com>
2021-09-23 16:52:37 -0400

    M1D10 Lecture prep

---
+++ module-1/10_Week2_Review/lecture-student/Week2Review/BugTrackerConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BugTrackerConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
