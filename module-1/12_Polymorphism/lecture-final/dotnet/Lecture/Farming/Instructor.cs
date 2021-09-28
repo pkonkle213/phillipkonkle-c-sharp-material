a80ecfefb831f4ddc7172661e1c0e068b2d54225
Matt Eland <matt.eland@gmail.com>
2021-09-28 15:18:56 -0400

    M1D12 Lecture Final

---
+++ module-1/12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Instructor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Instructor : ISingable
+    {
+        // FIELD - private
+        private string saying = "I'm allegedly not a sociopath";
+
+        // OLD-SCHOOL PROPERTY
+        public string Saying
+        {
+            get
+            {
+                return this.saying; // LOWERCASE
+            }
+            set
+            {
+                // value is the thing we're setting it = to
+                this.saying = value;
+            }
+        }
+
+        // AUTOMATIC PROPERTY - public
+        // may have a private / protected set
+        public string Name { get; set; } = "AN INSTRUCTOR";
+
+        public string MakeSoundOnce()
+        {
+            return "ANY QUESTIONS?";
+        }
+
+        public string MakeSoundTwice()
+        {
+            return Saying;
+        }
+    }
+}
