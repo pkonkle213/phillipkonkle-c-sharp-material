9fc45c3c57893ef6c59685e4f455a87507e31f69
Matt Eland <matt.eland@gmail.com>
2021-10-01 13:44:11 -0400

    M1D15 Lecture Final

---
+++ module-1/15_Week3_Review/lecture-final/BugTrackerConsoleApp/Feature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTrackerConsoleApp
+{
+    public class Feature : TrackedItem
+    {
+        public Feature(string summary)
+        {
+            this.Summary = summary;
+        }
+
+        public double HoursEstimated { get; set; }
+        public double HoursActual { get; set; }
+
+        public override string DisplayText
+        {
+            get
+            {
+                return $"{Id}) {Summary} (Feature)";
+            }
+        }
+    }
+}
