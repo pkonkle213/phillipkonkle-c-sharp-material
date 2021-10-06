fb4cd40c20c3fd7bda1cb4cb4cf7e4a633ae05ea
Matt Eland <matt.eland@gmail.com>
2021-10-06 13:41:08 -0400

    M1D17 Lecture Final

---
+++ module-1/18_Week4_Review/lecture-final/BugTrackerConsoleApp/Items/Bug.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTrackerConsoleApp.Items
+{
+    /// <summary>
+    /// Represents a bug in the system.
+    /// </summary>
+    public class Bug : TrackedItem
+    {
+        public Bug() : base("Unnamed Bug")
+        {
+
+        }
+
+        public Bug(string summary) : base(summary)
+        {
+        }
+
+        /// <summary>
+        /// The developer who summoned the bug into existence
+        /// </summary>
+        public string ResponsibleDev { get; set; } = "Matt Eland";
+
+        /// <summary>
+        /// The place in our code the bug exists (or likely exists)
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// The text to display to the user
+        /// </summary>
+        public override string DisplayText
+        {
+            get
+            {
+                string statusText = IsOpen 
+                    ? "Open"     // Value if IsOpen is true
+                    : "Closed";  // Value if IsOpen is false
+
+                return $"{Id}) {Summary} ({statusText})";
+            }
+        }
+    }
+}
---
+++ module-1/18_Week4_Review/lecture-final/BugTrackerConsoleApp/Items/Feature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTrackerConsoleApp.Items
+{
+    public class Feature : TrackedItem
+    {
+        public Feature(string summary) : base(summary)
+        {
+        }
+
+        public double HoursEstimated { get; set; }
+        public double HoursActual { get; set; }
+
+        public override string DisplayText
+        {
+            get
+            {
+                return $"{Id}) {Summary} (Feature)";
+            }
+        }
+    }
+}
