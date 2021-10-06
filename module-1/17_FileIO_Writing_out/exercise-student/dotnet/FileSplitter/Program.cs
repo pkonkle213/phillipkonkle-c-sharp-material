c8388af1ef1ed6da066e3b81263308da2dff0e46
Phillip Konkle <pkonkle213@gmail.com>
2021-10-06 08:51:02 -0400

    2021.10.06 - Homework and morning update

--- module-1/17_FileIO_Writing_out/exercise-student/dotnet/FileSplitter/Program.cs
+++ module-1/17_FileIO_Writing_out/exercise-student/dotnet/FileSplitter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileSplitter
 {
@@ -6,7 +7,42 @@ namespace FileSplitter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Where is the input file (please include the path to the file)? ");
+            string fileLocation = Console.ReadLine();
+
+            Console.Write("How many lines of text (max) should there be in the split files? ");
+            string linesOfSplitStr = Console.ReadLine();
+            int linesOfSplit = int.Parse(linesOfSplitStr);
+
+            int linesOfText = 0;
+            using (StreamReader reader = new StreamReader(fileLocation))
+            {
+                while (!reader.EndOfStream)
+                {
+                    reader.ReadLine();
+                    linesOfText++;
+                }
+            }
+
+            Console.WriteLine($"The input file has {linesOfText} lines of text.");
+            Console.WriteLine();
+            Console.WriteLine("Each file that is created must have a sequential number assigned to it.");
+            Console.WriteLine();
+            Console.WriteLine($"For a {linesOfText} 50 line input file \"input.txt\", this produces {linesOfText / linesOfSplit} output files.");
+            Console.WriteLine();
+            Console.WriteLine("**GENERATING OUTPUT**");
+            Console.WriteLine();
+
+            using (StreamReader reader = new StreamReader(fileLocation))
+            {
+                while (!reader.EndOfStream)
+                {
+                    for (int fileNumber = 0; fileNumber < linesOfText / linesOfSplit + 1; fileNumber++)
+                    {
+                        Console.WriteLine($"Generating input-{fileNumber + 1}.txt");
+                    }
+                }
+            }
         }
     }
 }
--- module-1/17_FileIO_Writing_out/exercise-student/dotnet/FizzWriter/Program.cs
+++ module-1/17_FileIO_Writing_out/exercise-student/dotnet/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,32 @@ namespace FizzWriter
     {
         public static void Main(string[] args)
         {
+            string currentDir = Environment.CurrentDirectory;
+            string fileLocation = currentDir + "\\Fizzbuzz.txt";
+            bool shouldAppend = false;
 
+            using (StreamWriter writer = new StreamWriter(fileLocation, shouldAppend))
+            {
+                for (int i = 1; i < 301; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        writer.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 5 == 0 || i.ToString().Contains("5"))
+                    {
+                        writer.WriteLine("Buzz");
+                    }
+                    else if (i % 3 == 0 || i.ToString().Contains("3"))
+                    {
+                        writer.WriteLine("Fizz");
+                    }
+                    else
+                    {
+                        writer.WriteLine(i);
+                    }
+                }
+            }
         }
     }
 }
