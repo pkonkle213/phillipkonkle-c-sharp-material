470f31d541a55cee5e358829f7a31fbd7cd7ca49
Matt Eland <matt.eland@gmail.com>
2021-10-17 14:15:25 -0400

    M2D6 Lecture starting point

---
+++ module-2/06_Database_Connectivity_DAO/lecture-student/dotnet/WorldGeography/DAL/CitySqlDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public class CitySqlDAO : ICityDAO
+    {
+        private readonly string connectionString;
+
+        private const string SqlInsert = "INSERT INTO city (name, countrycode, district, population) VALUES (@name, @countrycode, @district, @population);";
+        private const string SqlSelect = "SELECT id, name, countrycode, district, population FROM city WHERE countryCode = @countryCode;";
+
+        /// <summary>
+        /// Creates a new sql-based city dao.
+        /// </summary>
+        /// <param name="databaseconnectionString"></param>
+        public CitySqlDAO(string databaseconnectionString)
+        {
+            connectionString = databaseconnectionString;
+        }
+
+        public void AddCity(City city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<City> GetCitiesByCountryCode(string countryCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ module-2/06_Database_Connectivity_DAO/lecture-student/dotnet/WorldGeography/DAL/LanguageSqlDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public class LanguageSqlDAO : ILanguageDAO
+    {
+        private readonly string connectionString;
+
+        private const string SqlSelectByCountry = "SELECT countrycode, language, isofficial, percentage FROM countrylanguage WHERE countrycode = @countrycode";
+        private const string SqlInsert = "INSERT INTO countrylanguage (countrycode, language, isofficial, percentage) VALUES (@countrycode, @language, @isofficial, @percentage);";
+        private const string SqlDelete = "DELETE FROM countrylanguage WHERE countrycode = @countrycode AND language = @language;";
+
+        /// <summary>
+        /// Creates a sql based language dao.
+        /// </summary>
+        /// <param name="databaseConnectionString"></param>
+        public LanguageSqlDAO(string databaseConnectionString)
+        {
+            connectionString = databaseConnectionString;
+        }
+
+        public IEnumerable<Language> GetLanguages(string countryCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddNewLanguage(Language newLanguage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveLanguage(Language deadLanguage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
