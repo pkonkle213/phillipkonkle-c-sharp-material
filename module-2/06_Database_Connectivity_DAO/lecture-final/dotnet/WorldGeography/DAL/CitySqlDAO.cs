81db3f83e3a2c03552d78f44bc5221626cb30ddd
Matt Eland <matt.eland@gmail.com>
2021-10-18 14:19:01 -0400

    M2D6 Lecture Final

---
+++ module-2/06_Database_Connectivity_DAO/lecture-final/dotnet/WorldGeography/DAL/CitySqlDAO.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public class CitySqlDAO : ICityDAO
+    {
+        private readonly string connectionString;
+
+        private const string SqlInsert = 
+            "INSERT INTO city (name, countrycode, district, population) " +
+            "VALUES (@name, @countrycode, @district, @population); " +
+            "SELECT @@IDENTITY;"; // Select the ID of the row we just added
+
+        private const string SqlSelect = 
+            "SELECT id, name, countrycode, district, population " +
+            "FROM city " +
+            "WHERE countryCode = @countryCode;";
+
+        /// <summary>
+        /// Creates a new sql-based city dao.
+        /// </summary>
+        /// <param name="databaseconnectionString"></param>
+        public CitySqlDAO(string databaseconnectionString)
+        {
+            connectionString = databaseconnectionString;
+        }
+
+        public int AddCity(City city)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.connectionString))
+                {
+                    conn.Open(); // TODO: Talk about what happens if we don't do this, or the connection string is bad
+
+                    SqlCommand command = new SqlCommand(SqlInsert, conn);
+                    command.Parameters.AddWithValue("@name", city.Name);
+                    command.Parameters.AddWithValue("@countrycode", city.CountryCode);
+                    command.Parameters.AddWithValue("@district", city.District);
+                    command.Parameters.AddWithValue("@population", city.Population);
+
+                    int id = Convert.ToInt32(command.ExecuteScalar()); // Gets the first cell of the results. This is @@IDENTITY
+
+                    return id;
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Could not add city: " + ex.Message);
+                return -1; // No city
+            }
+        }
+
+        public IEnumerable<City> GetCitiesByCountryCode(string countryCode)
+        {
+            List<City> results = new List<City>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlSelect, conn);
+                    command.Parameters.AddWithValue("@countryCode", countryCode);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        City city = new City();
+
+                        city.CityId = Convert.ToInt32(reader["id"]);
+                        city.Name = Convert.ToString(reader["name"]);
+                        city.CountryCode = Convert.ToString(reader["countrycode"]);
+                        city.District = Convert.ToString(reader["district"]);
+
+                        // We only want to set the population if it isn't null in the data set
+                        if (reader["population"] != DBNull.Value)
+                        {
+                            city.Population = Convert.ToInt32(reader["population"]);
+                        }
+
+                        results.Add(city);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Could not query database " + ex.Message);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ module-2/06_Database_Connectivity_DAO/lecture-final/dotnet/WorldGeography/DAL/LanguageSqlDAO.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public class LanguageSqlDAO : ILanguageDAO
+    {
+        private readonly string connectionString;
+
+        private const string SqlSelectByCountry = 
+            "SELECT countrycode, language, isofficial, percentage " +
+            "FROM countrylanguage " +
+            "WHERE countrycode = @countrycode";
+
+        private const string SqlInsert = 
+            "INSERT INTO countrylanguage (countrycode, language, isofficial, percentage) " +
+            "VALUES (@countrycode, @language, @isofficial, @percentage);";
+
+        private const string SqlDelete = 
+            "DELETE FROM countrylanguage " +
+            "WHERE countrycode = @countrycode AND language = @language;";
+
+        private const string SqlNotGoingToUseThis = 
+            "SELECT * FROM " +
+            "countrylanguage " +
+            "WHERE countrycode = @countrycode";
+
+        /// <summary>
+        /// Creates a sql based language dao.
+        /// </summary>
+        /// <param name="databaseConnectionString"></param>
+        public LanguageSqlDAO(string databaseConnectionString)
+        {
+            this.connectionString = databaseConnectionString;
+        }
+
+        public IEnumerable<Language> GetLanguages(string countryCode)
+        {
+            List<Language> results = new List<Language>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlSelectByCountry, conn);
+                    command.Parameters.AddWithValue("@countrycode", countryCode);
+
+                    SqlDataReader reader = command.ExecuteReader(); // Queries the database and gives us a reader
+
+
+                    // Loop through each row in the results
+                    while (reader.Read())
+                    {
+                        Language lang = new Language();
+
+                        lang.CountryCode = Convert.ToString(reader["countrycode"]);
+                        lang.Name = Convert.ToString(reader["language"]);
+                        lang.IsOfficial = Convert.ToBoolean(reader["isofficial"]);
+                        lang.Percentage = Convert.ToInt32(reader["percentage"]); // 0.7 -> ???
+
+                        results.Add(lang);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Problem querying database " + ex.Message);
+            }
+
+            return results;
+        }
+
+        public bool AddNewLanguage(Language newLanguage)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand(SqlInsert, conn);
+                    command.Parameters.AddWithValue("@countrycode", newLanguage.CountryCode);
+                    command.Parameters.AddWithValue("@language", newLanguage.Name);
+                    command.Parameters.AddWithValue("@isofficial", newLanguage.IsOfficial);
+                    command.Parameters.AddWithValue("@percentage", newLanguage.Percentage);
+
+                    command.ExecuteNonQuery();
+                }
+
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Could not add language: " + ex.Message);
+                return false;
+            }
+        }
+
+        public bool RemoveLanguage(Language deadLanguage)
+        {
+            try
+            {
+                // Create a connection to the database
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open(); // Establish the connection
+
+                    /* SQL We're about to run:                      
+                    "DELETE FROM countrylanguage " +
+                    "WHERE countrycode = @countrycode AND language = @language;";
+
+                    Gets transformed to...
+
+                    DELETE FROM countrylanguage WHERE countrycode = 'USA' AND language = 'French';
+                     */
+
+                    // Build a command to run on the database
+                    SqlCommand command = new SqlCommand(SqlDelete, conn);
+                    command.Parameters.AddWithValue("@countrycode", deadLanguage.CountryCode);
+                    command.Parameters.AddWithValue("@language", deadLanguage.Name);
+
+                    // Execute the command on the database
+                    command.ExecuteNonQuery();
+                }
+
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Could not work with database: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
