970ae6f491d89200979772b214d62f38a7505e0a
Matt Eland <matt.eland@gmail.com>
2021-10-20 12:56:07 -0400

    M2D8 Lecture Final

---
+++ module-2/08_Data_Security/lecture-final/dotnet/DataSecurity.Cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using DataSecurity.Cli.DAL;
+using DataSecurity.Cli.Model;
+using DataSecurity.Cli.Security;
+using Microsoft.Extensions.Configuration;
+
+namespace DataSecurity.Cli
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = LoadConnectionString();
+
+            IPasswordHasher passwordHasher = new PasswordHasher();
+            IUserDao userDao = new UserSqlDAO(connectionString, passwordHasher);
+
+            UserManagerCli application = new UserManagerCli(userDao);
+
+            application.Run();
+        }
+
+        private static string LoadConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("UserManagerConnection");
+            return connectionString;
+        }
+    }
+}
