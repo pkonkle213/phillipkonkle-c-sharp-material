7b7fc3a4a16757905b9260d08e66c25d8e063618
Matt Eland <matt.eland@gmail.com>
2021-10-25 13:59:02 -0400

    M2D11 Lecture Final

---
+++ module-2/11_HTTP_Web_Services_GET/lecture-final/dotnet/HotelApp/ApiClients/HotelsApi.cs
@@ -0,0 +1,81 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace HTTP_Web_Services_GET_lecture.ApiClients
+{
+    public class HotelsApi
+    {
+        private readonly RestClient client;
+        private readonly string BASE_URL;
+
+        public HotelsApi(string laptopNumber)
+        {
+
+            // See https://te-mockauction-server.azurewebsites.net/swagger/index.html for API Documentation
+
+            this.BASE_URL = "https://te-mockauction-server.azurewebsites.net/students/" + laptopNumber + "/";
+            this.client = new RestClient();
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            RestRequest request = new RestRequest(BASE_URL + "hotels");
+
+            IRestResponse<List<Hotel>> response;
+            response = client.Get<List<Hotel>>(request);
+
+            List<Hotel> results = response.Data;
+            return results;
+        }
+
+        public List<Reservation> GetAllReservations()
+        {
+            RestRequest request = new RestRequest(BASE_URL + "reservations");
+
+            IRestResponse<List<Reservation>> response = client.Get<List<Reservation>>(request);
+
+            return response.Data;
+        }
+
+        public Hotel GetHotel(int hotelId)
+        {
+            // First, we need a RestClient (we already have one)
+            // If applicable, we need to set authentication information (next Monday's problem)
+
+            // Create a request to send over
+            // URL should look something like https://te-mockauction-server.azurewebsites.net/students/12345/hotels/5
+            RestRequest request = new RestRequest(BASE_URL + "hotels/" + hotelId);
+
+            //IRestResponse<Reservation> response = client.Get<Reservation>(request);
+
+            // Send the request and get a response
+            IRestResponse<Hotel> response;
+            response = client.Get<Hotel>(request); // Actually makes the GET request to the server and waits for a response
+
+            // Get the Hotel from the response
+            Hotel hotel = response.Data;
+
+            return hotel;
+
+            //return null;
+        }
+
+        public List<Reservation> GetReservationsForHotel(int hotelId)
+        {
+            RestRequest request = new RestRequest($"{BASE_URL}hotels/{hotelId}/reservations");
+            IRestResponse<List<Reservation>> response = client.Get<List<Reservation>>(request);
+
+            return response.Data;
+        }
+
+        public List<Hotel> GetAllHotelsInState(string state)
+        {
+            RestRequest request = new RestRequest($"{BASE_URL}hotels/filter?state={state}&isAwesome=1");
+            IRestResponse<List<Hotel>> response = client.Get<List<Hotel>>(request);
+
+            return response.Data;
+        }
+
+    }
+}
---
+++ module-2/11_HTTP_Web_Services_GET/lecture-final/dotnet/HotelApp/ApiClients/TravelApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HTTP_Web_Services_GET_lecture.Data;
+using RestSharp;
+
+namespace HTTP_Web_Services_GET_lecture.ApiClients
+{
+    public class TravelApi
+    {
+        public string GetRecommendedDestination()
+        {
+            // TODO: Call out to http://swapi.dev/api/planets/11/
+            RestClient client = new RestClient();
+
+            RestRequest request = new RestRequest("http://swapi.dev/api/planets/11/");
+
+            IRestResponse<Planet> response = client.Get<Planet>(request);
+
+            Planet planet = response.Data;
+
+            string ad = $"{planet.Name}. I hear it is " +
+                $"{planet.Climate} this time of year in its {planet.Terrain} lands";
+
+            return ad;
+        }
+    }
+}
