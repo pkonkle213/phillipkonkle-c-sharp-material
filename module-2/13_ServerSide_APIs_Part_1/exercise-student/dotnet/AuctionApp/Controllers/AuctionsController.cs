30f9b168dc0dbbe6046a363dd163d4eebe8bddd2
Phillip Konkle <pkonkle213@gmail.com>
2021-10-28 09:37:14 -0400

    Morning update

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Controllers/AuctionsController.cs
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Controllers/AuctionsController.cs
@@ -22,5 +22,47 @@ namespace AuctionApp.Controllers
                 dao = auctionDao;
             }
         }
+
+        /// <summary>
+        /// Get all auctions unless a search parameter is present
+        /// </summary>
+        [HttpGet("")]
+        public List<Auction> GetAllAuctions(string title_like, double currentBid_lte)
+        {
+            if (title_like == null && currentBid_lte == 0)
+            {
+                return dao.List();
+            }
+            else if (currentBid_lte == 0)
+            {
+                return dao.SearchByTitle(title_like);
+            }
+            else if (title_like == null)
+            {
+                return dao.SearchByPrice(currentBid_lte);
+            }
+            else
+            {
+                return dao.SearchByTitleAndPrice(title_like, currentBid_lte);
+            }
+        }
+
+        /// <summary>
+        /// Gets the auction with a specific ID
+        /// </summary>
+        [HttpGet("{id}")]
+        public Auction GetOneAuctionByID(int id)
+        {
+            return dao.Get(id);
+        }
+
+        /// <summary>
+        /// Add a new auction
+        /// </summary>
+        [HttpPost("")]
+        public Auction AddANewAuction(Auction auction)
+        {
+            return dao.Create(auction);
+        }
     }
 }
