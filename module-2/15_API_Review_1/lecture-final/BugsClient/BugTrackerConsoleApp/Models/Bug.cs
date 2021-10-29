aff4e02f921d8f178ee3a2a6b43de7dbd9363b69
Matt Eland <matt.eland@gmail.com>
2021-10-29 13:44:16 -0400

    M2D15 Review Day Final

---
+++ module-2/15_API_Review_1/lecture-final/BugsClient/BugTrackerConsoleApp/Models/Bug.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTrackerConsoleApp.Models
+{
+    /// <summary>
+    /// Represents a bug in the system.
+    /// </summary>
+    public class Bug
+    {
+        public Bug()
+        {
+
+        }
+
+        public Bug(string summary)
+        {
+            this.Summary = summary;
+        }
+
+        /// <summary>
+        /// The unique identifier for the bug
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The details associated with the bug
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// The date and time the bug was created
+        /// </summary>
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// Whether or not the bug is currently unresolved
+        /// </summary>
+        public bool IsOpen { get; set; } = true;
+
+        /// <summary>
+        /// The developer who summoned the bug into existence
+        /// </summary>
+        public string ResponsibleDev { get; set; } = "Matt Eland";
+
+        /// <summary>
+        /// The place in our code the bug exists (or likely exists)
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// The text to display to the user
+        /// </summary>
+        public string DisplayText
+        {
+            get
+            {
+                string statusText = IsOpen 
+                    ? "Open"     // Value if IsOpen is true
+                    : "Closed";  // Value if IsOpen is false
+
+                return $"{Id}) {Summary} ({statusText})";
+            }
+        }
+    }
+}
---
+++ module-2/15_API_Review_1/lecture-final/BugsClient/BugTrackerConsoleApp/Tasks.md
@@ -0,0 +1,23 @@
+﻿Tasks for Today
+
+- [x] Look over Sally the Squirrel
+- [x] Look over Norris Facts API
+- [x] Talk about AddSingleton vs AddTransient
+
+- [x] Create the Database
+- [x] Ensure Bugs Server is set to the startup project
+
+- [x] Decide What Endpoints You'll Need
+- [x] Implement Server Endpoints
+- [x] Test Server Endpoints
+
+- [x] Configure Project to Start both Client and Server
+- [x] Call API from Client
+
+- [ ] Create a new ASP .NET project
+- [x] Talk about NuGet Package Manager
+- [x] [Postman Galaxy Training](https://www.postman.com/postman/workspace/postman-galaxy-training/overview)
+
+- [ ] ???
+
+- [ ] Profit!
