74a5c50509078b1cf46381f307afedda3df5db99
Matt Eland <matt.eland@gmail.com>
2021-11-01 16:50:50 -0400

    M2D17 Lecture Student

---
+++ module-2/17_API_Review_2/lecture-student/BugsClient/BugTrackerConsoleApp/ApiClients/BugApiClient.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BugTrackerConsoleApp.Models;
+using RestSharp;
+
+namespace BugTrackerConsoleApp.ApiClients
+{
+    public class BugApiClient
+    {
+        private readonly RestClient client = new RestClient("https://localhost:44338/");
+
+        // NOTE: BaseAPI and RestClient aren't set for you. You'll need to create them
+
+        public bool CloseBug(int bugId)
+        {
+            RestRequest request = new RestRequest($"bugs/{bugId}");
+
+            var response = client.Delete<Bug>(request);
+
+            return response.ResponseStatus == ResponseStatus.Completed && response.IsSuccessful;
+
+            /* Get and a Put instead:
+            RestRequest request = new RestRequest($"bugs/{bugId}");
+            var response = client.Get<Bug>(request);
+
+            Bug bug = response.Data;
+
+            bug.IsOpen = false;
+
+            request = new RestRequest($"bugs/{bugId}");
+            request.AddJsonBody(bug);
+
+            response = client.Put<Bug>(request);
+
+            return response.Data != null;
+             */
+        }
+
+        public List<Bug> GetAllBugs()
+        {
+            RestRequest request = new RestRequest("bugs");
+
+            var response = client.Get<List<Bug>>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                Console.WriteLine("Could not communicate with the server");
+                return new List<Bug>();
+            }
+
+            if (!response.IsSuccessful)
+            {
+                Console.WriteLine("Could not get bugs. Error status code " + (int)response.StatusCode);
+                return new List<Bug>();
+            }
+
+            return response.Data;
+        }
+
+        public Bug AddBug(Bug bug)
+        {
+            RestRequest request = new RestRequest("bugs");
+            request.AddJsonBody(bug);
+
+            var response = client.Post<Bug>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                Console.WriteLine("Could not communicate with the server");
+                return null;
+            }
+
+            if (!response.IsSuccessful)
+            {
+                Console.WriteLine("Could not add bug. Error status code " + (int)response.StatusCode);
+                return null;
+            }
+
+            return response.Data;
+        }
+
+        // BONUS: Add a method for getting a single bug. You'll need to modify your UserInterface to call this as well.
+
+        // BONUS: Add a method for updating a bug. You'll need to modify your UserInterface to call this as well.
+    }
+}
---
+++ module-2/17_API_Review_2/lecture-student/BugsServer/Startup.cs
@@ -0,0 +1,81 @@
+using BugsServer.DAO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugsServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Adds swagger documentation file support. See https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.1&tabs=visual-studio for more details
+            ConfigureSwagger(services);
+
+            // DEPENDENCY INJECTION
+            string connectionString = Configuration.GetConnectionString("Bugs");
+            services.AddTransient<IBugDao>(sp => new BugSqlDao(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Adds swagger documentation file. See https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.1&tabs=visual-studio for more details
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                s.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "Bug Manager",
+                    Description = "Any observed bugs should be reported as features."
+                });
+            });
+        }
+
+    }
+}
