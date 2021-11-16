07a47e56baadb3c0f6692c5e85e7f8048c926831
Phillip Konkle <pkonkle213@gmail.com>
2021-11-15 19:23:22 -0500

    Homework AND the challenges!

--- module-3/05_Introduction_to_JavaScript/exercise-student/challenge-exercises.js
+++ module-3/05_Introduction_to_JavaScript/exercise-student/challenge-exercises.js
@@ -12,6 +12,38 @@
         iqTest("2 2 4 6") → 0 // all numbers are even, therefore there is no position of an odd number
 */
 
+function iqTest(stringOfNumbers) {
+    if (stringOfNumbers.length < 3) {
+        return 0;
+    }
+
+    let arrayOfNumbers = stringOfNumbers.split(" ");
+    let even=0;
+    let odd=0;
+
+    for (let i=0;i<arrayOfNumbers.length;i++) {
+
+        if (arrayOfNumbers[i]%2==0) {
+            even++;
+        }
+        if (arrayOfNumbers[i]%2==1) {
+            odd++;
+        }
+    }
+
+    for (let i=0;i<arrayOfNumbers.length;i++) {
+        if (even==1 && arrayOfNumbers[i]%2==0) {
+            return i+1;
+        }
+
+        if (odd==1 && arrayOfNumbers[i]%2==1) {
+            return i+1;
+        }
+    }
+
+    return 0;
+}
+
 /*
 2. **titleCase** Write a function that will convert a string into title case, given an optional 
     list of exceptions (minor words). The list of minor words will be given as a string with each 
@@ -28,3 +60,52 @@ argument is unused.
 		titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
         titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
 */
+
+function titleCase(title, ignore) {
+    // split the title into an array of words
+    let titleWords = title.split(" ");
+    
+    // capitalize the first letter of the first word
+    titleWords[0] = titleWords[0].substr(0,1).toUpperCase() + titleWords[0].substr(1,titleWords[0].length-1).toLowerCase();
+    
+    // check to see if there is an ignore list
+    if (ignore) {
+    
+        // split the ignore words into an array of words
+        let ignoreWords = ignore.split(" ");
+    
+        // if there is an ignore list, cycle through the words in the title to see if they're on the ignore list
+        for (let i=1;i<titleWords.length;i++) {
+            let onTheList = false;
+            for (let u=0;u<ignoreWords.length;u++) {
+                // check to see if the word is on the list
+                if (titleWords[i].toUpperCase()===ignoreWords[u].toUpperCase()) {
+                    onTheList = true;
+                }
+            }
+            // if on the list, lowercase the word
+            if (onTheList) {
+                titleWords[i] = titleWords[i].toLowerCase();
+            } else {
+            // if they're not on the list, capitalize the first letter and lowercase the rest
+                titleWords[i] = titleWords[i].substr(0,1).toUpperCase() + titleWords[i].substr(1,titleWords[i].length-1).toLowerCase();
+            }
+            
+        }
+    } else {
+        // if there is no ignore list, each word should have its first letter capitalized
+        for (let i=1;i<titleWords.length;i++) {
+            titleWords[i] = titleWords[i].substr(0,1).toUpperCase() + titleWords[i].substr(1,titleWords[i].length-1);   
+        }
+    }
+
+    let answer = "";
+    for (let i=0;i<titleWords.length-1;i++) {
+        answer += titleWords[i] + " ";
+    }
+
+    answer += titleWords[titleWords.length-1];
+
+    return answer;
+
+}
--- module-3/05_Introduction_to_JavaScript/exercise-student/exercises.js
+++ module-3/05_Introduction_to_JavaScript/exercise-student/exercises.js
@@ -9,6 +9,9 @@
 function sumDouble(x, y) {  // function #1 is defined for you. The rest you have to define yourself below
 	
 	// Note: This won't pass all tests yet. You still have work to do here.
+	if (x===y) {
+		return (x+y)*2;
+	}
 	return x + y;
 }
 
@@ -21,6 +24,18 @@ function sumDouble(x, y) {  // function #1 is defined for you. The rest you have
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
+function hasTeen(x,y,z) {
+	if (x > 12 && x < 20) {
+		return true;
+	}
+	if (y > 12 && y < 20) {
+		return true;
+	}
+	if (z > 12 && z < 20) {
+		return true;
+	}
+	return false;
+}
 
 /* 
 3. **lastDigit** Given two non-negative int values, return true if they have the same 
@@ -30,6 +45,12 @@ function sumDouble(x, y) {  // function #1 is defined for you. The rest you have
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
+function lastDigit(p,k) {
+	if (p%10===k%10) {
+		return true;
+	}
+	return false;
+}
 
 /*
 4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
@@ -39,6 +60,17 @@ function sumDouble(x, y) {  // function #1 is defined for you. The rest you have
 		seeColor("xxred") → ""
         seeColor("blueTimes") → "blue"
 */
+function seeColor(string) {
+	let answer = "";
+	if (string.slice(0,3)==="red"){
+		answer="red";
+	}
+	if (string.slice(0,4)==="blue"){
+		answer="blue";
+	}
+
+	return answer;
+}
 
 /*
 5. **oddOnly** Write a function that given an array of integer of any length, filters out 
@@ -47,6 +79,16 @@ function sumDouble(x, y) {  // function #1 is defined for you. The rest you have
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
+function oddOnly(numbers) {
+	let answer = [];
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i]%2==1){
+			answer.push(numbers[i]);
+		}
+	}
+
+	return answer;
+}
 
 /*
 6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
@@ -56,6 +98,15 @@ function sumDouble(x, y) {  // function #1 is defined for you. The rest you have
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
+function frontAgain(string) {
+	if (string.length == 2) {
+		return true;
+	}
+	if (string.substr(0,2)===string.substr(string.length-2,2)) {
+		return true;
+	}
+	return false;
+}
 
 /*
 7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
@@ -68,6 +119,17 @@ or false otherwise.
 		cigarParty(50, false) → true
 		cigarParty(70, true) → true
 */
+function cigarParty(num,weekend) {
+	if (weekend) {
+		return true;
+	}
+
+	if (num >= 40 && num <=60) {
+		return true;
+	}
+
+	return false;
+}
 
 /*
 8. **fizzBuzz** Because you know you can't live without it, FizzBuzz.
@@ -79,6 +141,19 @@ or false otherwise.
 		fizzBuzz(8) → 8
 */
 
+function fizzBuzz(number) {
+	if (number % 5 === 0 && number % 3 === 0) {
+		return "FizzBuzz";
+	}
+	if (number % 5 === 0) {
+		return "Buzz";
+	}
+	if (number % 3 === 0) {
+		return "Fizz";
+	}
+	return number;
+}
+
 /*
 9. **filterEvens** Write a function that filters an array to only include even numbers.
 
@@ -88,6 +163,17 @@ or false otherwise.
 	filterEvens([100, 8, 21, 24, 62, 9, 7]) → [100, 8, 24, 62]
 */
 
+function filterEvens(array) {
+	let answer = [];
+	for (let i=0;i<array.length;i++){
+		if(array[i]%2==0){
+			answer.push(array[i]);
+		}
+	}
+
+	return answer;
+}
+
 /*
 10. **filterBigNumbers** Write a function that filters numbers greater than or equal to 100.
 
@@ -96,6 +182,17 @@ or false otherwise.
 	filterBigNumbers([]) → []
 */
 
+function filterBigNumbers(array) {
+	let answer=[];
+	for (let i=0;i<array.length;i++){
+		if(array[i]>=100){
+			answer.push(array[i]);
+		}
+	}
+
+	return answer;
+}
+
 /*
 11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a 
 parameter, `x` passed in.
@@ -103,6 +200,17 @@ parameter, `x` passed in.
 	filterMultiplesOfX([3, 5, 1, 9, 18, 21, 42, 67], 3) → [3, 9, 18, 21, 42]
 	filterMultiplesOfX([3, 5, 10, 20, 18, 21, 42, 67], 5) → [5, 10, 20]
 */
+
+function filterMultiplesOfX(array, number) {
+	let answer = [];
+	for (let i=0;i<array.length;i++){
+		if(array[i]%number==0){
+			answer.push(array[i]);
+		}
+	}
+
+	return answer;
+}
 
 /*
 12. **createObject** Write a function that creates and returns an object with a property called 
@@ -115,3 +223,13 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+
+function createObject() {
+	const human = {
+		firstName: "Phillip",
+		lastName: "Konkle",
+		age: "18 with 16 years of experience"
+	}
+
+	return human;
+}
