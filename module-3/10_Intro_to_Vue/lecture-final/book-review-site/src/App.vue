e5aeb8b1aac2862bc73ccbf4a9ee04ab52cccd04
Matt Eland <matt.eland@gmail.com>
2021-11-22 13:56:32 -0500

    M3D10 Lecture Final

---
+++ module-3/10_Intro_to_Vue/lecture-final/book-review-site/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue.js</h1>
+    <h2>Intro</h2>
+    <p>This is a short intro</p>
+
+    <!-- <ProductReviews /> -->
+
+    <product-reviews />
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ProductReviews from './components/ProductReviews.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    ProductReviews
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
