4b0173446cd4324f023ce7c05e61ae5147ed1f26
Phillip Konkle <pkonkle213@gmail.com>
2021-12-02 20:27:51 -0500

    Another update

--- module-2/05_Database_Design/exercise-student/meetups.sql
+++ module-2/05_Database_Design/exercise-student/meetups.sql
@@ -34,6 +34,7 @@ CREATE TABLE member (
 	dateOfBirth date NOT NULL,
 	reminderEmails bit,
 	groupIn int NOT NULL,
+	eventIn int NOT NULL,
     CONSTRAINT pk_member_Number PRIMARY KEY (memberNumber),
 );
 
@@ -53,18 +54,18 @@ INSERT INTO interestGroup (groupNumber, groupName) VALUES (1,'Nonya');
 INSERT INTO interestGroup (groupNumber, groupName) VALUES (2,'Biznass');
 INSERT INTO interestGroup (groupNumber, groupName) VALUES (3,'HOLLA');
 
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (1,'Konkle','Phillip','sum1unopk@gmail.com','5132555099','8/20/1987',0,1);
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (2,'Teos','Kevin','waterboy@gmail.com','6148675309','7/4/1985',1,1);
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (3,'Kovalishin','Kulie','whateveryouwant@gmail.com','1234567890','10/10/1990',0,2);
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (4,'Eland','Matt','iWishIWasBatman@gmail.com','0987654321','1/1/1887',1,2);
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (5,'Wayne','Bruce','ImBatman@gmail.com','999999999','7/11/1957',1,3);
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (6,'Konkle','Ben','supersmart@gmail.com','6141234567','7/14/1979',0,3);
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (7,'ComeOn','Seriously','whycantIbedoneyet@gmail.com','999999999','12/25/1988',0,1);
-INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails,groupIn) VALUES (8,'Johnson','Dwayne','finally@gmail.com','1110800815','3/25/1977',0,2);
-
 INSERT INTO event (eventNumber, name, description, startDateTime, duration, groupRunning) VALUES (1,'Game Night','For squirrels after they''re done smoking','2021-01-01 00:00:00','60',2);
 INSERT INTO event (eventNumber, name, description, startDateTime, duration, groupRunning) VALUES (2,'Green Beer Day','Silly college students doing silly things','2021-03-16 00:00:00','120',1);
 INSERT INTO event (eventNumber, name, description, startDateTime, duration, groupRunning) VALUES (3,'Study Hour','Wonderful study buddies','2021-08-20 18:00:00','60',3);
 INSERT INTO event (eventNumber, name, description, startDateTime, duration, groupRunning) VALUES (4,'Retirement Party','Mainly because everyone will miss Kevin','2021-12-03 20:00:00','270',3);
+
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (1,'Konkle','Phillip','sum1unopk@gmail.com','5132555099','8/20/1987',0,1,2);
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (2,'Teos','Kevin','waterboy@gmail.com','6148675309','7/4/1985',1,1,1);
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (3,'Kovalishin','Kulie','whateveryouwant@gmail.com','1234567890','10/10/1990',0,2,3);
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (4,'Eland','Matt','iWishIWasBatman@gmail.com','0987654321','1/1/1887',1,2,2);
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (5,'Wayne','Bruce','ImBatman@gmail.com','999999999','7/11/1957',1,3,1);
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (6,'Konkle','Ben','supersmart@gmail.com','6141234567','7/14/1979',0,3,3);
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (7,'ComeOn','Seriously','whycantIbedoneyet@gmail.com','999999999','12/25/1988',0,1,2);
+INSERT INTO member (memberNumber, lastName, firstName, emailAddress, phoneNumber, dateOfBirth,reminderEmails, groupIn, eventIn) VALUES (8,'Johnson','Dwayne','finally@gmail.com','1110800815','3/25/1977',0,2,1);
 
 COMMIT TRANSACTION
